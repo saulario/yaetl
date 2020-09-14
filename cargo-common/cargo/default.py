6b7fec6484665e2eb4275af2e316f463d9cfa854
saulario <saul.correas@gmail.com>
2020-09-14 23:47:50 +0200

    Se incluye fichero de valores por defecto

---
+++ cargo-common/cargo/default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+
+#
+#
+#
+
+LOG_FORMAT = "%(asctime)s %(levelname)s %(threadName)s %(module)s.%(funcName)s %(message)s"
+
+#
+# Configuración de sesión
+#
+
+SESSION_DURATION=dt.timedelta(hours=1)
