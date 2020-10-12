57ccae19db3fbb9db5b50b05d7c7cca0c00bea1d
saulario <saul.correas@gmail.com>
2020-10-12 18:42:13 +0200

    Salvaguarda

--- cargo-api/api/app.py
+++ cargo-api/api/app.py
@@ -2,7 +2,7 @@
 import logging
 
 from flask import Flask, request
-from flask_restful import Api
+from flask_restful import Api, fields
 
 import cargo.default as default
 
@@ -14,8 +14,8 @@ log = logging.getLogger(__name__)
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(usu.List, "/inf/usu")
-api.add_resource(usu.Edit, "/inf/usu/<int:ususeq>")
+fields.Integer
 
-if __name__ == "__main__":
-    app.run(debug=True)
+api.add_resource(usu.Dispatcher, "/inf/usu",
+        "/inf/usu/<int:ususeq>")
+
--- cargo-api/api/resources/inf/usu.py
+++ cargo-api/api/resources/inf/usu.py
@@ -2,13 +2,65 @@
 
 import logging
 
-from flask import jsonify
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, marshal_with, fields
 
+import cargo.bl.basedal
 import cargo.bl.inf.usu
 import db
 
 log = logging.getLogger(__name__)
+
+payload = {
+    'action' : fields.String,
+    'body' : fields.Nested
+}
+
+class BaseDispatcher(Resource):
+
+    def __init__(self):
+        super().__init__()
+        self.bl = None
+
+    def put(self):
+        return self.post()
+
+    def post(self):
+        return None
+
+    def _get(self, conn, id):
+        log.debug("-----> Inicio")
+        return self.bl._read(conn, id)
+
+
+
+class Dispatcher(BaseDispatcher):
+
+    def put(self):
+        return self.post()
+
+    def gettty(self, ususeq):
+        log.info("-----> Inicio")
+        log.info(f"     (ususeq): {ususeq}")
+        return None
+
+    def get(self, ususeq):
+        dbi = db.get_db()
+        self.bl = cargo.bl.inf.usu.UsuBL(dbi.metadata)
+        retval = self._get(dbi.conn, ususeq)
+        dbi.conn.close()
+        return retval
+
+    def post(self):
+        log.info("-----> Inicio")
+        for form in request.form:
+            print(form)
+        return None
+
+    def delete(self, ususeq):
+        log.info("-----> Inicio")
+        log.info(f"     (ususeq): {ususeq}")
+        return None
 
 class List(Resource):
 
@@ -21,7 +73,3 @@ class List(Resource):
         log.info("<----- Fin")
         return jsonify(ususeq=usu.ususeq, usunom=usu.usunom) 
 
-class Edit(Resource):
-
-    def get(self, ususeq):
-        return ususeq
